47f8c6d5eea9f8be5e871eba14d7d778958024ff
sathish <gsathish8421@gmail.com>
2017-08-20 01:01:00 +0530

    added comments

--- Sudoku.java
+++ Sudoku.java
@@ -4,7 +4,7 @@ class Sudoku
 {
 	public static void main(String[] args) throws IOException{
 
-		int board[][]=new int[9][9];
+		int board[][]=new int[9][9]; 
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String line[];
 		for(int i=0;i<9;i++)
@@ -26,15 +26,16 @@ class Sudoku
 			{
 				if(board[row][col]==0)
 				{
+					//try 1 through 9 
 					for(int k=1;k<=9;k++)
 					{
 						if(isValid(board,row,col,k))
 						{
 							board[row][col]=k;
-							if(solve(board))
+							if(solve(board))//return true if it's solution else go back and replace with 0
 								return true;
 							else
-								board[row][col]=0;
+								board[row][col]=0;	
 
 						}
 					}
@@ -48,11 +49,11 @@ class Sudoku
 	{
 		for(int i=0;i<9;i++)
 		{
-			if(board[row][i]==k)
+			if(board[row][i]==k)//check if there is any repetition in row
 				return false;
-			if(board[i][col]==k)
+			if(board[i][col]==k)////check if there is any repetition in column
 				return false;
-			if(board[3*(row/3)+i/3][3*(col/3)+i%3]==k)
+			if(board[3*(row/3)+i/3][3*(col/3)+i%3]==k)//check if there is any repetition in 3*3 block
 				return false;
 		}
 		return true;
